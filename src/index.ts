1c5d2341c09df4297a256eb842dbc07e0634857b
Sebastian Alsina <alsinas@me.com>
2021-06-08 01:43:48 -0400

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+import Web3 from "web3";
+import fs from "fs";
+import path from "path";
+// @ts-ignore
+import erc721 from "./erc721.json";
+
+const provider = new Web3.providers.WebsocketProvider(
+    "wss://wsapi.fantom.network",
+    {
+        reconnect: {
+            auto: true,
+        },
+    }
+);
+const web3 = new Web3(provider);
+const contract = new web3.eth.Contract(
+    erc721.output.abi,
+    "0x3d7071E5647251035271Aeb780d832B381Fa730F"
+);
+
+async function downloadImage(url: any, path: any) {
+    const writer = fs.createWriteStream(`${path}/image.png`);
+
+    const response = await axios({
+        url,
+        method: "GET",
+        responseType: "stream",
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+        writer.on("finish", resolve);
+        writer.on("error", reject);
+    });
+}
+
+const init = async () => {
+    const totalSupply = await contract.methods.totalSupply().call();
+    console.log(totalSupply);
+
+    for (var i = 0; i < totalSupply; i++) {
+        try {
+            const path = `./backup/${i}`;
+            fs.mkdirSync(path);
+
+            const tokenURI = await contract.methods.tokenURI(i).call();
+
+            console.log(i, tokenURI);
+
+            const { data } = await axios.get(tokenURI);
+
+            await fs.writeFileSync(
+                `${path}/metadata.json`,
+                JSON.stringify(data, null, 4)
+            );
+            await fs.writeFileSync(
+                `${path}/links.txt`,
+                `${tokenURI}\r\n${data.image}`
+            );
+            await downloadImage(data.image, path);
+        } catch (error) {
+            console.error(`An error occurred while backing up #${i}.`);
+        }
+    }
+};
+
+init();
